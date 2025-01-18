da9a77236dcf369ce7a443b2c4ea0df890a5e365
FRANCESCO PAPPATERRA <113634131+SPAPppp@users.noreply.github.com>
2025-01-18 10:49:15 +0100

    Delete website directory

--- website/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-views=Blueprint('views', __name__)
-
-# created home route for the website
-@views.route('/')
-def home():
-    return render_template("home.html")
